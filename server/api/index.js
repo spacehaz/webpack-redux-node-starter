b72174343c29e0f33d5124ea50f7d061d3bd73a8
Haz Baykulov <hazbaykulov@MacBook-Pro-Haz.local>
2018-06-12 14:27:50 +0300

    added minor fixes

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,11 @@
+const db = require('../db')
+
 module.exports = app => {
   app.get('/api/v1/user', (req, res) => {
     res.json({'foo': 'bar'})
+  })
+
+  app.get('/api/v1/user/:name', (req, res) => {
+    console.log(db.get())
   })
 }
--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const app = express()
 const config = require('../config')
 const routes = require('./routes')
 const api = require('./api')
+const db = require('./db')
 
 app.use(express.static('app'))
 routes(app)
 api(app)
+
 app.listen(config.port, () => {
   console.log('server is running on port: ', config.port)
 })
